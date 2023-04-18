fd7b23ae67cb218a463c2c62bbea25bfd926eb76
reNer2468 <69228985+reNer2468@users.noreply.github.com>
2023-04-18 21:36:59 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -19,9 +19,9 @@ def Output_Graph():
 
 def Plot_Hist(x,color,title,label_name):
     plt.hist(x,stacked=True,bins=30,color=color,label=label_name,alpha=0.3,ec="black")
-    plt.title(title+"のヒストグラム")
+    plt.title(title)
     plt.xlabel(title)
-    plt.ylabel("出現数")
+    plt.ylabel("occurrence")
     plt.legend()
     graph=Output_Graph()
     return graph
