3a1a15eabd6f51853a3529d61d770bd4c81c47ca
reNer2468 <69228985+reNer2468@users.noreply.github.com>
2023-04-18 21:37:45 +0900

    Update views.py

--- views.py
+++ views.py
@@ -9,5 +9,5 @@ def index(request):
     Searched_Data1=[data.ViewCount for data in Total_Data]
     Searched_Data2=[data.ChatRate for data in Total_Data]
     
-    context={"Analize_ViewCount":Plot_Hist(Searched_Data1,"blue","視聴数","視聴数"),"Analize_ChatRate":Plot_Hist(Searched_Data2,"red","チャット数","チャット数")}
+    context={"Analize_ViewCount":Plot_Hist(Searched_Data1,"blue","viewcount","viewcount"),"Analize_ChatRate":Plot_Hist(Searched_Data2,"red","chatrate","chatrate")}
     return render(request,"analize_data/analysis.html",context)
